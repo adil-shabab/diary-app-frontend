1a1331ca6146fbb08f60193a0546868393a257df
Adil Shabab <adilshaba@gmail.com>
2022-07-20 07:32:54 -0700

    stylesheet created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./style.css";
+import Header from "./components/Header/Header";
+import NoteListPage from "./pages/NoteListPage";
+import Notepage from "./pages/Notepage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<NoteListPage />} />
+          <Route path="/notes/:id" element={<Notepage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
